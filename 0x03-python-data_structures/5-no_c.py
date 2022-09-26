41a41c2a43c62f2a4e86ae7cc374ce325ec7ec61
AndrewGithinji <andrewgithinji@hotmail.com>
2022-09-26 20:46:33 +0300

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     update_str = ''
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if i != 'c' and 'C' != i:
             update_str += i
             return update_str
